d45d2ebd7c904c21fe032956cd01d6eb08f54ee2
Jyotiradityashin <87648668+Jyotiradityashin@users.noreply.github.com>
2021-11-11 10:18:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+
+
+function preload() {
+//preload the images here
+
+}
+
+function setup() {
+  createCanvas(3000, 800);
+  // create sprites here
+
+}
+
+function draw() {
+  background(0);
+
+}
+
+function mouseDragged() {
+  Matter.Body.setPosition(hero.body, { x: mouseX, y: mouseY});
+  }
+
+  strokeWeight(0);
+
+
+
+
+
+
